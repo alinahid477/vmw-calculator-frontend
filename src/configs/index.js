b7a3fa2bd43c0e43ffff11487d6574b2282c93b8
Ali Nahid <alinahid@gmail.com>
2021-07-09 18:29:36 +1000

    console log config

--- src/configs/index.js
+++ src/configs/index.js
@@ -1,4 +1,5 @@
 const env = process.env.NODE_ENV || 'development';
+console.log(env);
 const config = require(`./environments/${env.toLowerCase()}`).default;
-
+console.log(config);
 export default config;
